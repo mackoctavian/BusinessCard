1dffca55a4c05a6d088658af4fada8010b75d841
Mack Octavian <maestrooctavian@gmail.com>
2024-12-15 06:42:57 +0300

    Rendered native images

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Image source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg"}} 
+      style={{width: "100%", aspectRatio: 16/9}}/>
+      <Image source={require('./assets/vadim.png')} 
+      style={{width: 150, height: 150, borderRadius: 150, borderColor: "white", borderWidth: 5, marginTop: -75}}/>
+      <Text style={{fontSize: 30, fontWeight: "bold"}}>Vadim Savin</Text>
+      <Text>Founder of notJust.dev</Text>
       <StatusBar style="auto" />
     </View>
   );
