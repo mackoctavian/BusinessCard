082226fc4396f796bf62bbf73b9217d43440c0c4
Mack Octavian <maestrooctavian@gmail.com>
2024-12-17 08:45:30 +0300

    Finalization

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView, Button, Linking} from 'react-native';
-import {FontAwesome6} from "@expo/vector-icons"
+import { StyleSheet, Text, View, Image, ScrollView, Button, Linking, ActivityIndicator } from 'react-native';
+import { FontAwesome6 } from "@expo/vector-icons"
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -9,45 +9,55 @@ export default function App() {
     Linking.openURL('mailto:maestrooctavian@gmail.com')
   }
 
+  const links = {
+    github: 'SavinVa'
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']}>
-    <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.container}>
-      <Image source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg"}} 
-      style={{width: "100%", aspectRatio: 16/9}}/>
-      <Image source={require('./assets/vadim.png')} 
-      style={{width: 150, height: 150, borderRadius: 150, borderColor: "white", borderWidth: 5, marginTop: -75}}/>
-      <Text style={{fontSize: 30, fontWeight: "bold"}}>Vadim Savin</Text>
-      <Text>Founder of notJust.dev</Text>
-      
-      <View style={{flexDirection: 'row', marginVertical: 10, gap: 10}}>
-      <FontAwesome6 name="github" size={24} color="black" />
-      <FontAwesome6 name="x-twitter" size={24} color="black" />
-      <FontAwesome6 name="at" size={24} color="black" />
-      </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.container}>
+            <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg" }}
+              style={{ width: "100%", aspectRatio: 16 / 9 }} />
+            <Image source={require('./assets/vadim.png')}
+              style={{ width: 150, height: 150, borderRadius: 150, borderColor: "white", borderWidth: 5, marginTop: -75 }} />
+            <Text style={{ fontSize: 30, fontWeight: "bold" }}>Vadim Savin</Text>
+            <Text>Founder of notJust.dev</Text>
 
-      <Button title='Contact me' onPress={onContactMe}/>
+            <renderIcons />
 
-      <Text style={{padding: 10, fontSize: 16, lineHeight: 20}}>
-        Quis nisi nisi velit Lorem. Ipsum consequat Lorem labore esse ea est nostrud qui. Voluptate qui voluptate sunt est. Duis est nulla ea amet deserunt. Sit fugiat cupidatat eiusmod ea consectetur pariatur eiusmod irure enim veniam consequat.
+            <Button title='Contact me' onPress={onContactMe} />
 
-Ad anim dolor cillum occaecat Lorem et laboris amet enim. Veniam esse dolore cupidatat ut incididunt est sint. Esse nisi proident eu minim Lorem. Consequat magna sit excepteur minim dolor in deserunt adipisicing qui sunt excepteur minim. Nostrud mollit laborum cillum nulla irure cupidatat consequat ea.
+            <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+              Quis nisi nisi velit Lorem. Ipsum consequat Lorem labore esse ea est nostrud qui. Voluptate qui voluptate sunt est. Duis est nulla ea amet deserunt. Sit fugiat cupidatat eiusmod ea consectetur pariatur eiusmod irure enim veniam consequat.
 
-Cupidatat ea commodo fugiat aliqua duis consequat velit culpa ullamco voluptate ullamco ex excepteur occaecat. Lorem ea culpa laboris exercitation voluptate ipsum pariatur. Laborum tempor commodo in culpa nostrud exercitation fugiat tempor aute sunt. Proident sunt ut sint in sint in ex minim ex occaecat do consectetur est. Duis magna voluptate do et. Cillum non tempor anim commodo aliqua sunt amet deserunt.
+              Ad anim dolor cillum occaecat Lorem et laboris amet enim. Veniam esse dolore cupidatat ut incididunt est sint. Esse nisi proident eu minim Lorem. Consequat magna sit excepteur minim dolor in deserunt adipisicing qui sunt excepteur minim. Nostrud mollit laborum cillum nulla irure cupidatat consequat ea.
 
-Cillum deserunt pariatur incididunt aute mollit excepteur nostrud laborum aliquip sunt. Est qui non sit aliqua velit sint ea ipsum consectetur id non sit mollit. Sunt minim nulla quis reprehenderit.
+              Cupidatat ea commodo fugiat aliqua duis consequat velit culpa ullamco voluptate ullamco ex excepteur occaecat. Lorem ea culpa laboris exercitation voluptate ipsum pariatur. Laborum tempor commodo in culpa nostrud exercitation fugiat tempor aute sunt. Proident sunt ut sint in sint in ex minim ex occaecat do consectetur est. Duis magna voluptate do et. Cillum non tempor anim commodo aliqua sunt amet deserunt.
 
-Ea eiusmod reprehenderit nostrud occaecat ipsum elit mollit aute elit esse eiusmod. Cupidatat officia cupidatat laborum eiusmod Lorem do sunt commodo ullamco incididunt. Lorem eu esse sunt nulla labore proident culpa quis reprehenderit proident. Nulla enim cupidatat adipisicing non laboris aliquip. Est est amet sint laborum excepteur consectetur laboris irure enim aliquip laborum velit occaecat.
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-    </ScrollView>
-    </SafeAreaView>
+              Cillum deserunt pariatur incididunt aute mollit excepteur nostrud laborum aliquip sunt. Est qui non sit aliqua velit sint ea ipsum consectetur id non sit mollit. Sunt minim nulla quis reprehenderit.
+
+              Ea eiusmod reprehenderit nostrud occaecat ipsum elit mollit aute elit esse eiusmod. Cupidatat officia cupidatat laborum eiusmod Lorem do sunt commodo ullamco incididunt. Lorem eu esse sunt nulla labore proident culpa quis reprehenderit proident. Nulla enim cupidatat adipisicing non laboris aliquip. Est est amet sint laborum excepteur consectetur laboris irure enim aliquip laborum velit occaecat.
+            </Text>
+            <StatusBar style="auto" />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 
+}
 
+
+const renderIcons = () => {
+  return (
+    <View style={{ flexDirection: 'row', marginVertical: 10, gap: 10 }}>
+      <FontAwesome6 name='github' size={24} color='black' />
+      <FontAwesome6 name='x-twitter' size={24} color='black' />
+      <FontAwesome6 name='at' size={24} color='black' />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
